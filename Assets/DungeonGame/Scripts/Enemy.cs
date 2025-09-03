5871b637aada183e573fb2e9c4d1d826233fb132
lucassilb <lucassilveribomtempo@gmail.com>
2025-09-03 12:04:07 -0300

    atu

---
+++ Assets/DungeonGame/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Enemy: MonoBehaviour 
+{
+    public string nome;
+    public float vida;
+    public float speed;
+    public float maxVida;
+    public Transform target;
+
+    private void Start()
+    {
+        vida = maxVida;
+        Introduction();
+    }
+    void Introduction()
+    {
+        Debug.Log("O nome é" + nome +
+            "a vida é" + vida + "e o maximo de vida é "
+           + maxVida);
+
+    }
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            target.position,
+            speed * Time.deltaTime);
+    }
+    private void Update()
+    {
+        Move();
+    }
+}
